4678f73922970cbf9487a9da154407ea2de782a5
Shareni <anubis.firepoi@gmail.com>
2021-08-13 17:43:50 +0200

    initial

---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/shareni/.local/share/omf/themes/slavic-cat/fish_prompt.fish
---
+++ fish/functions/lofi.fish
@@ -0,0 +1,3 @@
+function lofi --wraps='mpv --no-video --volume="30" "https://www.youtube.com/watch?v=5qap5aO4i9A"' --description 'alias lofi mpv --no-video --volume="30" "https://www.youtube.com/watch?v=5qap5aO4i9A"'
+  mpv --no-video --volume="30" "https://www.youtube.com/watch?v=5qap5aO4i9A" $argv; 
+end
